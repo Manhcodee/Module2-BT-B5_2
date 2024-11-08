b66d56a4a8b5458b7da77b1a771309c65d666676
Manhcodee <phammanh2952@gmail.com>
2024-11-08 16:44:47 +0700

    first

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+//    public char Test() {
+//       System.out.println("Student: " + getName() + " " + "Class: " + getClasses());
+//        return 0;
+//    }
+}
